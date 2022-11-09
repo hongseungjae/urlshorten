36b35e90700c46a78b3df2c53a33818d035f9094
hongseungjae <ghdtmdwo123@naver.com>
2022-11-09 23:25:17 +0900

    Initial commit

---
+++ src/main/java/com/hong/urlshorten/UrlshortenApplication.java
@@ -0,0 +1,13 @@
+package com.hong.urlshorten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong/urlshorten/UrlshortenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.urlshorten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
